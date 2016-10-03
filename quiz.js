9b6936b4ff9a6ea28407e5a4dafefe5879e3a6c9
Helana Nosrat <helanan@gmail.com>
2016-10-03 18:12:59 -0500

    added javascript components

--- quiz.js
+++ quiz.js
@@ -1,8 +1,43 @@
 function populatePage (inventory) { 
-// Loop over the inventory and populate the page 
+var selectedCar = '';
+      		for (var i=0; i<selectedCar.inventory.length; i++) {
+      			//redo all of these to match
+      			selectedCar += '<div class="inventory">';
+      			selectedCar += '<img src=" ' + selectedCar.inventory[i].make + '" ';
+      			selectedCar += 'alt="' + selectedCar.inventory[i].model + '" />';
+      			selectedCar += '<p><b>' + selectedCar.inventory[i].year + '</b><br>';
+      			selectedCar += selectedCar.inventory[i].price + '</p>';
+      			selectedCar += '</div>';
+      		}
+      		//REMEMBER TO USE BOOTSTRAP ELEMENTS
+      		document.getElementById('content').innerHTML = newContent;
+      	}
+      });
+    xhr.open('GET', 'inventory.json', true);
+    xhr.send(null);
+    }
+  };
+
+})();
+
+
+
+
+
 // Now that the DOM is loaded, establish all the event listeners
 needed CarLot.activateEvents(); } 
-// Load the inventory and send a callback
-function to be 
-// invoked after the process is complete
+
 CarLot.loadInventory();
+
+
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+   
+
+  function () {
+      	if(xhr.status === 200) {
+      		responseObject = JSON.parse(xhr.responseText);
+
+      		
