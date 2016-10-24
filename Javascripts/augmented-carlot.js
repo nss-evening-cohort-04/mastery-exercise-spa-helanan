d449eaf7caaa5e80f4f7f0218cacfce7e70760d5
Helana Nosrat <helanan@gmail.com>
2016-10-24 17:21:10 -0500

    added new code

--- Javascripts/augmented-carlot.js
+++ Javascripts/augmented-carlot.js
@@ -1,15 +1,22 @@
-(function (CarLot) {
-	var rows = function makeRows() {
-		carDescription.forEach(function(loadInventory) {
-			var row = '<div></div>';
-			row.append.textContent(car.make);
-			row.append.textContent(car.model);
-			row.append.textContent(car.year);
-			row.append.textContent(car.price);
-			row.append.textContent(car.description);
-			rows.push({
-				make: make,
-				model: model,
-   
-      document.getElementById('selectedCar').innerHTML = newContent; }
-  )};
+var Inventory = (function(previousInventory) {
+	
+	previousInventory.changeCard = function(makeCard) {
+		var cards = previousInventory.loadCards();
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].classList.remove("on");
+		}
+		card.classList.add("on");
+	};
+
+	previousInventory.innerHTML = function(makeCard) {
+		var cards = previousInventory.loadCards();
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].classList.remove("description");
+		}
+		makeCard.classList.add("description");
+	};  
+
+	return previousInventory;
+
+})(Inventory);
+
