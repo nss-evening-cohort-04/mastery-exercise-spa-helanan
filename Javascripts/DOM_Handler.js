7005fcf56b6a02043c724d2061236893936d7d26
Helana Nosrat <helanan@gmail.com>
2016-10-24 17:20:50 -0500

    redid indexhtml

--- Javascripts/DOM_Handler.js
+++ Javascripts/DOM_Handler.js
@@ -1,77 +1,29 @@
-var cars = (function(){
+function createDOM (carInventory) {
+  var carOutput = "";
+  var carCard = document.getElementById("container");
 
-  
-  function executeThisCodeIfXHRFails () {
-    console.log("An error occurred while transferring");
-    }
+  for (var i = 0; i < carInventory.length; i++) {
+    if (i % 3 === 0) { 
+      carOutput += `<div class= "rows">`;
+    } 
 
-  function executeThisCodeWhenChunksArrive () {
-    }
-  var inventoryDiv = document.getElementById("container");
-
-  function executeThisCodeAfterFileLoaded () {
-    var data = JSON.parse(this.responseText);
-    var inventoryData = "";
-    var currentInventory;
-    data.cars.reverse();
-
-  for (var i = 0; i < data.cars.length; i++) {
-      currentModel = data.cars[i].model;
-      currentMake = data.cars[i].make;
-      currentYear = data.cars[i].year;
-      currentPrice = data.cars[i].price;
-      currentDescription = data.cars[i].description;
-        inventoryDetails += '<div class="container">';
-        inventoryDetails += '<div class="inventory">';
-        inventoryDetails += '<div class="col-sm-4">';
-        inventoryDetails += '<div class="list-group">';
-        inventoryDetails += '<div class="panel panel-primary">';
-        // inventoryDetails += '<img src=" "';
-      inventoryDetails += `<div id='${currentModel}'>`;
-         inventoryDetails += `<div id='${currentMake}'>`;
-         inventoryDetails += `<div id='${currentYear}'>`;
-         inventoryDetails += `<div id='${currentPrice}'>`;
-          inventoryDetails += `<div id='${currentDescription}'>`;
-      inventoryDetails +=`</div> + </div> + </div> + </div> + </div>`;
-
-   };
-
-    inventoryDiv.innerHTML = inventoryDetails;
-  }
-
-   inventoryDiv.addEventListener("click",function(e){
-    console.log("target",e.target);
-    Car.removeInventory(e.target);
-    Car.oldInventory(e.target);
-    Car.removeEachInventory(e.target.id);
-
-   });
-
-  
-
-  var myRequest = new XMLHttpRequest();
-
-
-  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-  myRequest.addEventListener("error", executeThisCodeIfXHRFails)
-  myRequest.addEventListener("progress", executeThisCodeWhenChunksArrive)
-  myRequest.open("GET", "inventory.json")
-  myRequest.send();
-
-  return {
-
-    oldInventory : function(target){
-      for(var i = 0; i < 5; i++){
-        if (target.id ===( 'deleteBtn-${i}')){
-          console.log(target.parentNode);
-          target.parentNode.parentNode.removeChild(target.parentNode);
+    carOutput += `<div class="col-md-4" style="border: 6px ${carInventory[i].make}">
+                  <h3>${carInventory[i].model}</h3>
+                  <p>${carInventory[i].year}</p>
+                  <p>${carInventory[i].price}</p>,
+                  <p>${carInventory[i].available}</p>
+                  <p>${carInventory[i].make}</p>
+                  <p>${carInventory[i].available}</p>
+                  <p class="descripton">${carInventory[i].description}</p></div>`;
+    
+    if ((i + 1) % 3 === 0) { 
+      carOutput += `</div>`;
         }
       }
-    }
-  };
 
-})();
+  carCard.innerHTML = carOutput;
+ 
+  Inventory.activateEvents();
+};
 
-
-
-
+Inventory.loadCar(createDOM);
