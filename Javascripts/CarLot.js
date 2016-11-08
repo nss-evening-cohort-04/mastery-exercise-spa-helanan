c1a0f2603f1cb0cb60761b548b172936590bcded
Helana Nosrat <helanan@gmail.com>
2016-11-07 21:01:50 -0600

    added event listeners, etc. still not loading properly

--- Javascripts/CarLot.js
+++ Javascripts/CarLot.js
@@ -1,54 +1,31 @@
 //original javascript
 var CarLot = (function(carModifier){ //carlot modifier
 
- console.log("carModifier", carModifier);
+ // console.log("carModifier", carModifier);
 
   carModifier.populatePage = function(cars){
-    console.log("cars", cars);
-    // do stuff
     var carsToPage = document.getElementById("car-loaded"); //gets element for card output
     textInputUpdater = document.getElementById("textInputUpdaterEl");//get text input area
-    outPutString = "", //set output string to empty
-    carDescription = "", //sets description string to empty
-    carCardContainer = "", //Car card, set to empty
-    carAvailable = "", //availability to empty
-    counter = 1; //sets a counter element for each object to value of 1
+    outPutString = ""; //set output string to empty
+    carDescription = ""; //sets description string to empty
+    carCardContainer = ""; //Car card, set to empty
+    carAvailable = ""; //availability to empty
 
-    //Loop through inventory array and populate text to page
-    for (var i=0; i<cars.length; i++) {//for loop increment by 1 through the length of the inventory array
-
+    for(var i=0; i<cars.length; i++) {//for loop increment by 1 through the length of the inventory array
       //availability function for cars
       if (cars[i].purchased === false) { //IF Else statement for availability if inventory has a method of purchased then not in stock
         carAvailable = "In Stock"; // if in stock set carAvailable to string of instick
-         } else { 
-            carAvailable = "Out of Stock"; //otherwise set car availabile to string of out of stock
-        }
+      } else { 
+        carAvailable = "Out of Stock"; //otherwise set car availabile to string of out of stock
+      }
       //function 4: output to DOM, find selectors for each data key value and instert into string with divs
       
-            outPutString += `<div class="col-md-4 carCard" id="carCard--${counter}" style="border:3px solid">` 
-               outPutString += `<div class="row">`
-                    outPutString += `<div class="container-fluid carContainer">`
-                      outPutString += `<div id="carCardClass">`
-                        outPutString += `<div class="panel-heading panel-custom" id="make">${cars[i].make} ${cars[i].model}</div>`
-                          outPutString += `<div id="year">${cars[i].year}</div><hr>`
-                              outPutString += `<div class="price"><em><b>Price</em>: $</b>${cars[i].price}</div>`
-                                outPutString += `<div class="description"><b>Description:</b> ${cars[i].description}</div>`
-                                  outPutString += `<div class="availability">${carAvailable}</div>`
-                                  outPutString += `<div class="panel-footer panel-custom">`
-                                  outPutString += `</div>`
-                                outPutString += `</div>`
-                              outPutString += `</div>`
-                        outPutString += `</div>`
-                      outPutString += `</div>`
-      if ((i+1) % 3 === 0) {  //if divisible by 3 is false add a div to output string
-                  outPutString += '</div>';
-        //do i need an extra parantheses at end of function?
-
-        carsToPage.innerHTML = outPutString;  //writes CarCards to the DOM
-        counter ++;
-      };
+      outPutString += `<div class="col-md-4 carCard" id="carCard--${i}" style="border:3px solid">${cars[i].make} <br> ${cars[i].model} <br> ${cars[i].year} <br> ${cars[i].price} <br>${cars[i].description}<br>${carAvailable}</div>`
+      outPutString += `</div>`
+      carsToPage.innerHTML = outPutString;  //writes CarCards to the DOM
+      CarLot.activateEvents();
     }
-    console.log("carsToPage", carsToPage);
+    // console.log("carsToPage", carsToPage);
   }   
   return carModifier;
 
